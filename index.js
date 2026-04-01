22c3c6c7cf33e593f8df96a741d448aad0e0e4a5
Sonia <soniangaruiya@gmail.com>
2026-04-01 12:32:46 +0300

    Update file

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+// calculate Tax
+function calculateTax(amount) {
+  return amount * 0.1;
+}
 
+console.log(calculateTax(1000))
 
+// Convert to uppercase
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+console.log(convertToUpperCase("sonia")); 
 
+// Find Maximum
 
-// This is required for the test to function properly  
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+console.log(findMaximum(5, 10)); 
+
+//Is palindrome
+
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+
+console.log(isPalindrome("madam"))
+
+//Calculate discounted price
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = (discountPercentage / 100) * originalPrice;
+  return originalPrice - discount;
+}
+
+console.log(calculateDiscountedPrice(1000,10))
+
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
